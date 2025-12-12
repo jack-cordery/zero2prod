7784e4656939fd8c39df521c1ffe54334d83c28f
Jack Cordery <jack.redolf@gmail.com>
2025-12-12 14:59:55 +0000

    port abstracting

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, dev::Server, web};
 
 async fn greet(req: HttpRequest) -> impl Responder {
@@ -9,13 +11,13 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-pub fn run() -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health", web::get().to(health_check))
     })
-    .bind("127.0.0.1:8000")?
+    .listen(listener)?
     .run();
     Ok(server)
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,22 +1,25 @@
+use std::net::TcpListener;
+
 use zero2prod::run;
 
 #[tokio::test]
 async fn test_health_check() {
-    spawn_app();
+    let addr = spawn_app();
 
     let client = reqwest::Client::new();
 
-    let response = client
-        .get("http://127.0.0.1:8000/health")
-        .send()
-        .await
-        .expect("should return");
+    let full_addr = format!("{addr}/health");
+    let response = client.get(full_addr).send().await.expect("should return");
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app() {
-    let server = run().expect("should spin up");
+fn spawn_app() -> String {
+    let addr = "127.0.0.1";
+    let listener = TcpListener::bind(format!("{addr}:0")).expect("should bind");
+    let port = listener.local_addr().expect("should be valid").port();
+    let server = run(listener).expect("should spin up");
     tokio::spawn(server);
+    format!("http://{addr}:{port}")
 }
