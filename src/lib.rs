ca63878cf11cc19d995f87aa8f7e1d351320f81a
Jack Cordery <jack.redolf@gmail.com>
2025-12-15 11:12:16 +0000

    subcription end point

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,23 @@ use std::net::TcpListener;
 
 use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, dev::Server, web};
 
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
     format!("Hello {name}")
 }
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscriptions(form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
@@ -16,6 +26,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
         App::new()
             .route("/", web::get().to(greet))
             .route("/health", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscriptions))
     })
     .listen(listener)?
     .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -15,6 +15,57 @@ async fn test_health_check() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscription_returns_400_for_invalid_form_data() {
+    let addr = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let full_addr = format!("{addr}/subscriptions");
+
+    let test_cases = vec![
+        ("name=jack%20cordery", "missing email"),
+        ("email=jack%40gmail.com", "missing name"),
+        ("", "missing both email and name"),
+    ];
+
+    for (invalid_body, test_name) in test_cases {
+        let response = client
+            .post(&full_addr)
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("should return");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The api failed to deliver a status code of 400 whilst testing {}",
+            test_name
+        );
+    }
+}
+
+#[tokio::test]
+async fn subscribe_returns_200_for_valid_form_data() {
+    let addr = spawn_app();
+
+    let client = reqwest::Client::new();
+
+    let full_addr = format!("{addr}/subscriptions");
+
+    let response = client
+        .post(&full_addr)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body("name=jack%20cordery&email=jack%40gmail.com")
+        .send()
+        .await
+        .expect("should return");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
 fn spawn_app() -> String {
     let addr = "127.0.0.1";
     let listener = TcpListener::bind(format!("{addr}:0")).expect("should bind");
