ef8fd47556d92547d9ef963ac37993957821c27f
Jack Cordery <jack.redolf@gmail.com>
2025-12-22 18:57:10 +0000

    better logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,6 +12,9 @@ pub async fn subscriptions(
     form: web::Form<FormData>,
     connection: web::Data<PgPool>,
 ) -> HttpResponse {
+    log::info!("Adding new subscriber {} {}", form.name, form.email);
+
+    log::info!("Saving subscriber to the DB");
     match sqlx::query!(
         r#"
 INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
@@ -24,9 +27,12 @@ INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $
     .execute(connection.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New subscription successfully added!");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {e}");
+            log::error!("Failed to execute query: {e:?}");
             HttpResponse::InternalServerError().finish()
         }
     }
