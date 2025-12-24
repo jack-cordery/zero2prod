83cdf0829c8dd63f9d39e8db99a498cff9833754
Jack Cordery <jack.redolf@gmail.com>
2025-12-24 17:08:44 +0000

    typo

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,14 +12,14 @@ pub struct FormData {
 
 #[instrument(name = "Adding a new subscriber", skip(form, connection), fields(request_id = %Uuid::new_v4(), subscriber_email = %form.email, subscriber_name = %form.name))]
 pub async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgPool>) -> HttpResponse {
-    match insert_subsriber(&connection, &form).await {
+    match insert_subscriber(&connection, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
 #[instrument(name = "Inserting new subcriber to DB", skip(connection, form))]
-pub async fn insert_subsriber(connection: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+pub async fn insert_subscriber(connection: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
 INSERT INTO subscriptions (id, email, name, subscribed_at) VALUES ($1, $2, $3, $4)
