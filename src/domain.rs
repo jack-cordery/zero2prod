c618b4e7f47e0d7ee106f240465acf99beed7ef0
Jack Cordery <jack.redolf@gmail.com>
2026-01-08 16:54:27 +0000

    add unit tests to domain

--- src/domain.rs
+++ src/domain.rs
@@ -5,6 +5,7 @@ pub struct NewSubcsriber {
     pub name: SubscriberName,
 }
 
+#[derive(Debug)]
 pub struct SubscriberName(String);
 
 impl SubscriberName {
@@ -19,7 +20,7 @@ impl SubscriberName {
         let invalid_chars = ['/', '{', '}', '(', ')', '\\', '"'];
         let contains_invalid_chars = s.chars().any(|c| invalid_chars.contains(&c));
         if is_empty_or_whitespace || is_too_long || contains_invalid_chars {
-            Err(format!("{s} is an invalid string for SubscriberName"))
+            Err(format!("{s} is not a valid SubscriberName."))
         } else {
             Ok(Self(s))
         }
@@ -29,5 +30,46 @@ impl SubscriberName {
 impl AsRef<str> for SubscriberName {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use claim::{assert_err, assert_ok};
+
+    use super::*;
+
+    #[test]
+    fn test_parse_returns_error_given_empty_string() {
+        let subscriber_name = SubscriberName::parse("".to_string());
+        assert_err!(subscriber_name);
+    }
+    #[test]
+    fn test_parse_returns_error_given_whitespace() {
+        let subscriber_name = SubscriberName::parse("  ".to_string());
+        assert_err!(subscriber_name);
+    }
+    #[test]
+    fn test_parse_returns_error_given_invalid_chars() {
+        let invalid_chars = ['/', '{', '}', '(', ')', '\\', '"'];
+        for ic in invalid_chars {
+            let subscriber_name = SubscriberName::parse(ic.to_string());
+            assert_err!(subscriber_name);
+        }
+    }
+    #[test]
+    fn test_parse_returns_result_given_valid_name() {
+        let subscriber_name = SubscriberName::parse("jack cordery".to_string());
+        assert_ok!(subscriber_name);
+    }
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "a".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
     }
 }
