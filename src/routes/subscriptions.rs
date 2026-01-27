2784776f87027d3a798863d6624318482293740b
Jack Cordery <jack.redolf@gmail.com>
2026-01-27 16:58:17 +0000

    Adding tracing on confirmation email error

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -68,18 +68,20 @@ pub async fn subscribe(
         return HttpResponse::InternalServerError().finish();
     };
 
-    if send_confirmation_email(
+    match send_confirmation_email(
         &email_client,
         new_subscriber,
         &application_base_url.0,
         &subscription_token,
     )
     .await
-    .is_err()
     {
-        return HttpResponse::InternalServerError().finish();
+        Err(e) => {
+            tracing::error!("Failed to send confirmation email {e:?}");
+            return HttpResponse::InternalServerError().finish();
+        }
+        Ok(_) => return HttpResponse::Ok().finish(),
     }
-    HttpResponse::Ok().finish()
 }
 
 #[instrument(
