78044bf91790d503d1137e026b88ef527dbf81ae
Jack Cordery <jack.redolf@gmail.com>
2026-02-28 08:38:19 +0000

    spawn non-blocking thread for verify password, which by design is an expensive operation and thus blocking

--- src/routes/newsletter.rs
+++ src/routes/newsletter.rs
@@ -109,9 +109,31 @@ pub async fn get_strored_credentials(
     Ok(optional_row.map(|row| (row.user_id, SecretString::from(row.password_hash))))
 }
 
+#[tracing::instrument(
+    name = "Verifying password",
+    skip(expected_password_hash, password_candidate)
+)]
+pub fn verify_password(
+    expected_password_hash: SecretString,
+    password_candidate: SecretString,
+) -> Result<(), PublishError> {
+    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
+        .context("Failed to parse hashed password from PHC string")
+        .map_err(PublishError::UnexpectedError)?;
+    tracing::info_span!("Verify password hash")
+        .in_scope(|| {
+            Argon2::default().verify_password(
+                password_candidate.expose_secret().as_bytes(),
+                &expected_password_hash,
+            )
+        })
+        .context("Failed to verigy password")
+        .map_err(PublishError::AuthError)
+}
+
 #[tracing::instrument(name = "Validate credentials", skip(credentials, pool))]
 pub async fn validate_credentials(
-    credentials: &Credentials,
+    credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
     let (user_id, expected_password_hash) = get_strored_credentials(&credentials.username, pool)
@@ -119,19 +141,12 @@ pub async fn validate_credentials(
         .map_err(PublishError::UnexpectedError)?
         .ok_or_else(|| PublishError::AuthError(anyhow!("Unknown username.")))?;
 
-    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
-        .context("Failed to parse hashed password from PHC string")
-        .map_err(PublishError::UnexpectedError)?;
-
-    tracing::info_span!("Verify password hash")
-        .in_scope(|| {
-            Argon2::default().verify_password(
-                credentials.password.expose_secret().as_bytes(),
-                &expected_password_hash,
-            )
-        })
-        .context("Invalid password.")
-        .map_err(PublishError::AuthError)?;
+    tokio::task::spawn_blocking(move || {
+        verify_password(expected_password_hash, credentials.password)
+    })
+    .await
+    .context("Spawning blocking thread failed.")
+    .map_err(PublishError::UnexpectedError)??;
     Ok(user_id)
 }
 
@@ -144,7 +159,7 @@ pub async fn publish_newsletter(
 ) -> Result<HttpResponse, PublishError> {
     let credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
     tracing::Span::current().record("username", &credentials.username);
-    let user_id = validate_credentials(&credentials, &pool).await?;
+    let user_id = validate_credentials(credentials, &pool).await?;
     tracing::Span::current().record("user_id", user_id.to_string());
     let confirmed_subscriptions = get_confirmed_subscriptions(&pool).await?;
     for sub in confirmed_subscriptions {
