59e23ff04f626bee21cb828fac9077be059a4815
Jack Cordery <jack.redolf@gmail.com>
2026-05-06 06:58:09 +0100

    updated rate_limit test to try and pass the gha with a slower cpu

--- tests/api/rate_limit.rs
+++ tests/api/rate_limit.rs
@@ -5,14 +5,12 @@ use crate::helpers::spawn_app_with_config;
 #[tokio::test]
 pub async fn server_returns_429_when_rate_limit_is_breached() {
     let rate_limit = RateLimitSettings {
-        rate_limit: 10,
+        rate_limit: 1,
         namespace: "rate-limit-test".into(),
     };
     let test_app = spawn_app_with_config(rate_limit).await;
-    for _ in 0..10 {
-        let response = test_app.get_admin_dashboard().await;
-        assert_ne!(429, response.status().as_u16());
-    }
+    let response = test_app.get_admin_dashboard().await;
+    assert_ne!(429, response.status().as_u16());
     let response = test_app.get_admin_dashboard().await;
     assert_eq!(429, response.status().as_u16());
 }
